21d325f012cd1d2d4fd8f3a321d0f0d66a894932
Michael Schilling <maschill92@gmail.com>
2014-04-14 14:28:26 -0500

    Set up initial project structure

--- Teacher/TeacherInterface.cs
+++ Teacher/TeacherInterface.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Nitro
+namespace Teacher
 {
-    public partial class Form1 : Form
+    public partial class TeacherInterface : Form
     {
-        public Form1()
+        public TeacherInterface()
         {
             InitializeComponent();
         }
