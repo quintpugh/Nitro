17e0d6710963abd1d4328b74a587ccbc57e1e013
Ben Carpenter <benjamin.carpenter@my.und.edu>
2014-05-01 16:14:57 -0500

    Documented the student class and exercise class for the student module.

--- Student/Exercise.cs
+++ Student/Exercise.cs
@@ -7,12 +7,24 @@ using MySql.Data.MySqlClient;
 
 namespace Student
 {
+    /// <summary>
+    /// This is the exercise class of the student module.
+    /// It will generate a list of all the exercises that the student needs to perform.
+    /// It fields are the unique id for the exercise, the name of the exercise, and the exercise text
+    /// </summary>
     class Exercise
     {
         public int id;
         public String name { get; set; }
         public String text { get; set; }
 
+        /// <summary>
+        /// This is the constructor for the Exercise class.
+        /// It initializes the exercises object's information.
+        /// </summary>
+        /// <param name="id">The unique id of the exercise</param>
+        /// <param name="n">The name of the exercise</param>
+        /// <param name="t">The text for the exercise</param>
         public Exercise(int id, String n, String t)
         {
             this.id = id;
@@ -20,6 +32,13 @@ namespace Student
             text = t;
         }
 
+        /// <summary>
+        /// This is a static function to genereate a list of exercise objects that the student needs to perfrom.
+        /// It uses the username and class id of the student in order to query the database about which exercises the
+        /// student has not performed.
+        /// </summary>
+        /// <param name="s">The student object of the student who is currently logged in.</param>
+        /// <returns>A list of exercises the student needs to perform.</returns>
         public static List<Exercise> ToBePerformed(Student s)
         {
             MySqlDataReader reader = MySQL_Manager.MySqlManager.Instance.ExecuteReader
--- Student/Student.cs
+++ Student/Student.cs
@@ -7,6 +7,10 @@ using MySql.Data.MySqlClient;
 
 namespace Student
 {
+    /// <summary>
+    /// This class creates the student user object after they log into the system.
+    /// The class queries the database for the student's class, name, and password
+    /// </summary>
     public class Student
     {
         public String username;
@@ -15,6 +19,13 @@ namespace Student
         public String lName;
         public int classId;
 
+        /// <summary>
+        /// This is the constructor for the student class.
+        /// It contains all the functionality of the class.
+        /// It is passed the username of the student from the login module, and
+        /// uses it to query the student's information.
+        /// </summary>
+        /// <param name="uName">The username of the student sent by the login module</param>
         public Student(String uName)
         {
             this.username = uName;
