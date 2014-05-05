f8bf5358a3011b44f7afb29087a9c14948b51d61
Ben Carpenter <benjamin.carpenter@my.und.edu>
2014-05-04 21:59:02 -0500

    Documented ExerciseResult for student module

--- Student/ExerciseResult.cs
+++ Student/ExerciseResult.cs
@@ -7,6 +7,10 @@ using MySql.Data.MySqlClient;
 
 namespace Student
 {
+    /// <summary>
+    /// The exercise result class creates an object with all the information about
+    /// the exercise the student just completed, and adds that information to the database.
+    /// </summary>
     class ExerciseResult
     {
         private Student student;
@@ -14,6 +18,14 @@ namespace Student
         private int errorCount;
         private int time;
 
+        /// <summary>
+        /// This is the constructor for the class.  It initializes
+        /// the variables to the student's exercise result information.
+        /// </summary>
+        /// <param name="s">The student who completed the exercise</param>
+        /// <param name="exId">The id of the exercise completed</param>
+        /// <param name="errCount">The number of errors produced by the student</param>
+        /// <param name="time">The time taken to complete the exercise</param>
         public ExerciseResult(Student s, int exId, int errCount, int time)
         {
             this.student = s;
@@ -22,6 +34,10 @@ namespace Student
             this.time = time;
         }
 
+        /// <summary>
+        /// Attempts to add the exercise result to the database.
+        /// </summary>
+        /// <returns>True if the exercise result was successfully added to the database, false otherwise</returns>
         public bool Add()
         {
             return MySQL_Manager.MySqlManager.Instance.ExecuteNonQuery 
